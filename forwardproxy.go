235e0fba50d3c503f98f91a838cb17a94e57e318
klzgrad <kizdiv@gmail.com>
2020-06-13 16:30:59 +0800

    Ignore old padding format of "..."

--- forwardproxy.go
+++ forwardproxy.go
@@ -332,7 +332,9 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyht
 			fallthrough
 		case 3:
 			defer r.Body.Close()
-			return dualStream(targetConn, r.Body, w, r.Header.Get("Padding") != "")
+			padding := r.Header.Get("Padding")
+			// Ignores old client padding of "..."
+			return dualStream(targetConn, r.Body, w, padding != "" && padding[0] != '.')
 		}
 
 		panic("There was a check for http version, yet it's incorrect")
